4a850e44c9d9f17576fb57bb7922d730342c9cef
maxwelliverson <54956548+maxwelliverson@users.noreply.github.com>
2023-12-27 15:20:16 -0800

    Added intrusive list type

---
+++ src/agate/list.hpp
@@ -0,0 +1,117 @@
+//
+// Created by Maxwell on 2023-11-30.
+//
+
+#ifndef AGATE_INTERNAL_LIST_HPP
+#define AGATE_INTERNAL_LIST_HPP
+
+namespace agt {
+
+
+
+  struct list_node {
+    list_node* next;
+    list_node* prev;
+  };
+
+
+  struct flist_node {
+    flist_node* next;
+  };
+
+
+
+  class raw_flist {
+    flist_node*  m_head;
+    flist_node** m_tail;
+  public:
+
+    raw_flist() : m_head(nullptr), m_tail(&m_head) { }
+
+
+    void push_back(flist_node* node) noexcept {
+      *m_tail = node;
+      m_tail = &node->next;
+    }
+
+    void push_front(flist_node* node) noexcept {
+      if (m_head == nullptr)
+        m_tail = &node->next;
+      node->next = m_head;
+      m_head = node;
+    }
+
+    void pop_front() noexcept {
+      m_head = m_head->next;
+      if (m_head == nullptr)
+        m_tail = &m_head;
+    }
+
+    [[nodiscard]] flist_node* front() const noexcept {
+      return m_head;
+    }
+  };
+
+  template <typename T>
+  concept flist_capable = std::derived_from<T, object> && requires(T* obj, T*& lval)
+  {
+    obj->next = obj;
+    lval = obj->next;
+    atomicCompareExchange(obj->next, lval, obj);
+  };
+
+
+  template <typename T>
+  class flist {
+  public:
+    using pointer = T*;
+
+    static_assert(flist_capable<T>);
+
+
+    flist() = default;
+
+
+    [[nodiscard]] uint32_t size() const noexcept {
+      return m_size;
+    }
+
+    [[nodiscard]] bool empty() const noexcept {
+      return m_size == 0;
+    }
+
+    void push_back(pointer obj) noexcept {
+      *m_tail = obj;
+      m_tail  = &obj->next;
+      ++m_size;
+    }
+
+    void push_front(pointer obj) noexcept {
+      if (m_head == nullptr)
+        m_tail = &obj->next;
+      obj->next = m_head;
+      m_head = obj;
+      ++m_size;
+    }
+
+    void pop_front() noexcept {
+      m_head = m_head->next;
+      if (m_head == nullptr)
+        m_tail = &m_head;
+      --m_size;
+    }
+
+    [[nodiscard]] pointer front() const noexcept {
+      return m_head;
+    }
+
+  private:
+    pointer   m_head = nullptr;
+    pointer*  m_tail = &m_head;
+    uint32_t  m_size = 0;
+  };
+
+
+}
+
+#endif //AGATE_INTERNAL_LIST_HPP
